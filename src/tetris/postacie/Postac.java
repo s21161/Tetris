994129efbcb2f4d1c77986ea25d96d1c0a62eb1e
s21161 <56189637+s21161@users.noreply.github.com>
2020-06-13 01:04:32 +0200

    Update Postac.java

--- src/tetris/postacie/Postac.java
+++ src/tetris/postacie/Postac.java
@@ -2,7 +2,7 @@ package tetris.postacie;
 public class Postac {
     public int[][] tablica2; // = new int[4][4];
     public int[][][] tablica; // kształt obrócony
-    public int log; //kąt kształtu, 0-3
+    public int log; 
 
     public void eksportTablicy() {
         tablica2 = tablica[(log++) % 4];
